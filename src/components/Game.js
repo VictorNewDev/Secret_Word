db776066dcf505e526813bc55b0c962d61ea1c2c
VictorNewDev <victor.passos2030@gmail.com>
2024-07-12 14:45:09 -0300

    Commit inicial do projeto

---
+++ src/components/Game.js
@@ -0,0 +1,45 @@
+import "./Game.css";
+
+const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score,}) => {
+  const[letter, setLetter] = useState("")
+  
+  return (
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: {score}</span>
+      </p>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>{pickedCategory}</span>
+      </h3>
+      <p>Você ainda tem {guesses} tentativa(s).</p>
+      <div className="wordContainer">
+        {letters.map((letter, i) => (
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">
+              {letter}
+            </span>
+
+          ) : (
+            <span key={i} className="blankSquare"></span>
+          )
+        ))}
+      </div>
+      <div className="letterContainer">
+        <p>Tente adinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required onChange={(e) => setLetter(e.target.value)}/>
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        {wrongLetters.map((letter, i) => (
+         <span key={i}>{letter}, </span>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Game
